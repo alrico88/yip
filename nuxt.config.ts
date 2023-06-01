36e3f315759a835959c73890e5e86d3585d4c0ff
Alberto Rico <alrico88@gmail.com>
2023-06-01 23:37:38 +0200

    feat: added fonts to workbox cache

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -83,7 +83,7 @@ export default defineNuxtConfig({
     },
     workbox: {
       navigateFallback: "/",
-      globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
+      globPatterns: ["**/*.{js,css,html,png,svg,ico,woff,woff2}"],
       cleanupOutdatedCaches: true,
     },
   },

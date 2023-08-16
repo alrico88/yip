d2692bf686150130e280f63ce410d05f393a249e
Alberto Rico <alrico88@gmail.com>
2023-08-16 19:36:03 +0200

    style: changed ui colors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { name: "theme-color", content: "#ebf4f5" },
+        { name: "theme-color", content: "#e0e7ff" },
         { name: "author", content: "Alberto Rico" },
         {
           name: "keywords",
@@ -62,7 +62,7 @@ export default defineNuxtConfig({
     manifest: {
       name: "YiP",
       description: "Web app to track your mood throughout the year",
-      theme_color: "#ebf4f5",
+      theme_color: "#e0e7ff",
       icons: [
         {
           src: "pwa-192x192.png",
